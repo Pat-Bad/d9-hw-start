51e75edc28aa4ef9a38d066cc3add33000b104bc
Patricia <patricia.badji89@gmail.com>
2024-12-09 16:07:08 +0100

    updates

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mainReducer from "../reducers";
 
-const myReduxStore = configureStore({ reducer: mainReducer });
+const store = configureStore({ reducer: mainReducer });
 
-export default myReduxStore;
+export default store;
