87a823371739835832e87103483844500714dacd
Patricia <patricia.badji89@gmail.com>
2024-12-10 16:28:11 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import MainSearch from "./components/MainSearch";
 import CompanySearchResults from "./components/CompanySearchResults";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Favourites from "./components/Favourites";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route
           path="/"
